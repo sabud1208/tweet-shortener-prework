50d968a3b58b2cd3b9bd435e508bac31e6cbcc85
Stephanie Messad <sabud1208@gmail.com>
2018-12-11 00:59:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+dictionary= {
+"hello" => 'hi',
+"to, two, too" => '2',
+"for, four" =>  "4",
+'be' =>  'b',
+'you' =>  'u',
+"at" => "@",
+"and" =>  "&",
+}
+def word_substituter(tweet)
+  tweet.map 
+  
+
+end
